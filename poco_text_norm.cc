20388db0bf7aac582d0bc2cc5e89565070a35122
amir <amir.harati@jibo.com>
2018-02-14 14:04:16 -0800

    replace raw pointer with uniq_ptr

--- poco_text_norm.cc
+++ poco_text_norm.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <memory>
 #include "Poco/RegularExpression.h"
 
 using namespace std;
@@ -19,11 +20,11 @@ public:
 void read_rules(const string& input_file_a, map<string, string>& rules_a);
 // complie rules for future use
 void compile_rules(const map<string, string>& rules_a, map<string,
- RegularExpression*> &compiled_rules_a);
+ unique_ptr<RegularExpression>> &compiled_rules_a);
 // normlaize the input text based on rules
 void text_normalize(const string& input_str_a, string& output_a,
  const map<string, string>& rules_a,
-  const map<string, RegularExpression*> &compiled_rules_a);
+  const map<string, unique_ptr<RegularExpression>> &compiled_rules_a);
 
 
 void read_rules(const string& input_file_a, map<string, string>& rules_a) {
@@ -33,24 +34,25 @@ void read_rules(const string& input_file_a, map<string, string>& rules_a) {
 }
 
 void compile_rules(const map<string, string>& rules_a, map<string,
- RegularExpression*> &compiled_rules_a) {
+ unique_ptr<RegularExpression>> &compiled_rules_a) {
   //vector<RegWrapper> rw;
-  map<string, RegularExpression*> rw;
+  //map<string, RegularExpression*> rw;
   for (const auto& k : rules_a) {
-    RegularExpression *re = new RegularExpression(k.first);
+    // RegularExpression *re = new RegularExpression(k.first);
+    unique_ptr<RegularExpression> re(new RegularExpression(k.first));
     //compiled_rules_a[k.first] = &re;
-    compiled_rules_a[k.first] = re;
+    compiled_rules_a[k.first] = std::move(re);
   }
   return;
 }
 
 void text_normalize(const string& input_str_a, string& output_a,
  const map<string, string>& rules_a,
- const map<string, RegularExpression*> &compiled_rules_a) {
+ const map<string, unique_ptr<RegularExpression>> &compiled_rules_a) {
   string s = "Who’s amir is 20 abc Who's ";
   for (const auto& k : compiled_rules_a) {
-    RegularExpression *re = k.second;
-    re->subst(s, "'");
+    //RegularExpression *re = k.second;
+    k.second->subst(s, "'");
     std::cout<<s<<std::endl;
   }
   return;
@@ -60,7 +62,7 @@ int main(int argc, char** argv) {
   map<string, string> rules;
   string input_str = "";
   string output = "";
-  map<string, RegularExpression*> compiled_rules;
+  map<string, unique_ptr<RegularExpression>> compiled_rules;
   read_rules("", rules);
   for (auto & k : rules) {
     cout << k.first << " " << k.second << endl;
